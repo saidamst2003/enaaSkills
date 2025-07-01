4e65b4ad6ae1e66dbeb48716fa428ea8ec003c09
saidamst2003 <saida2003mst@gmail.com>
2025-07-01 12:22:46 +0100

    j'ai creer entité souscompetence

---
+++ src/main/java/delivery/example/enaaskill/model/SousCompetence.java
@@ -0,0 +1,42 @@
+package delivery.example.enaaskill.model;
+
+
+import jakarta.persistence.*;
+@Entity
+public class SousCompetence {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nom;
+
+    private boolean validee = false;
+
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public boolean isValidee() {
+        return validee;
+    }
+
+    public void setValidee(boolean validee) {
+        this.validee = validee;
+    }
+}
+
