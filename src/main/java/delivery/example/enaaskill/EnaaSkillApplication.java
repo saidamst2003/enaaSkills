bb307a8363ad7b5c9002b597b93550611371016c
saidamst2003 <saida2003mst@gmail.com>
2025-07-01 12:11:23 +0100

    first commit

---
+++ src/main/java/delivery/example/enaaskill/EnaaSkillApplication.java
@@ -0,0 +1,13 @@
+package delivery.example.enaaskill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnaaSkillApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnaaSkillApplication.class, args);
+    }
+
+}
---
+++ src/test/java/delivery/example/enaaskill/EnaaSkillApplicationTests.java
@@ -0,0 +1,13 @@
+package delivery.example.enaaskill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnaaSkillApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
