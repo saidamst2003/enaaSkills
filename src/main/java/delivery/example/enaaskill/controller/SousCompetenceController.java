5206207018df87a04cb93b7f1108d39acb9d24c7
saidamst2003 <saida2003mst@gmail.com>
2025-07-02 16:27:07 +0100

    j'ai creer souscompetence controller

--- src/main/java/delivery/example/enaaskill/controller/SousCompetenceController.java
+++ src/main/java/delivery/example/enaaskill/controller/SousCompetenceController.java
@@ -11,7 +11,7 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/api/sous-competences")
 public class SousCompetenceController {
-
+   l
     private final SousCompetenceService sousCompetenceService;
 
     public SousCompetenceController(SousCompetenceService sousCompetenceService) {
