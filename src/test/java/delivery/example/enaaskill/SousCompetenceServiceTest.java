ba2b6cfb87ee38859b022e1651796026d176dbda
saidamst2003 <saida2003mst@gmail.com>
2025-07-03 17:45:29 +0100

    j'ai creer tinjecter les objectif

---
+++ src/test/java/delivery/example/enaaskill/SousCompetenceServiceTest.java
@@ -0,0 +1,43 @@
+package delivery.example.enaaskill;
+
+
+import delivery.example.enaaskill.DTO.SousCompetenceDTO;
+import delivery.example.enaaskill.mapper.SousCompetenceMapper;
+import delivery.example.enaaskill.model.Competence;
+import delivery.example.enaaskill.model.SousCompetence;
+import delivery.example.enaaskill.repository.SousCompetenceRepository;
+import delivery.example.enaaskill.service.CompetenceService;
+import delivery.example.enaaskill.service.SousCompetenceService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.*;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class SousCompetenceServiceTest {
+
+    @Mock
+    private SousCompetenceRepository sousCompetenceRepository;
+
+    @Mock
+    private SousCompetenceMapper sousCompetenceMapper;
+
+    @Mock
+    private CompetenceService competenceService;
+
+    @InjectMocks
+    private SousCompetenceService sousCompetenceService;
+
+    @BeforeEach
+    void setup() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+
+}
+
